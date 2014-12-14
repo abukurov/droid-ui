01e2a7ec8d5bfe32b11ecd558480c8ce04c82032
abukurov <abukurov@hwdtech.ru>
2014-12-14 17:26:26 +0700

    implement widget removing and fix styles

--- public/js/controllers/controllers.js
+++ public/js/controllers/controllers.js
@@ -4,7 +4,7 @@ var angular = require('angular');
 var _ = require('lodash');
 
 module.exports = angular.module('controllers', [])
-  .controller('mockup', ['$scope', '$http', function ($scope, $http) {
+  .controller('mockup', ['$scope', '$http', '$document', function ($scope, $http, $document) {
     $http.get('/configs')
       .success(function (widgets) {
           $scope.allWidgets = _.map(widgets, function (widget) {
@@ -30,5 +30,19 @@ module.exports = angular.module('controllers', [])
             model: widget
           };
         });
-    }
+    };
+
+    $document.bind('keydown', function onKeyDown($event) {
+
+      if ($scope.selectedWidget && $event.which === 46) {
+        var index = $scope.widgets.indexOf($scope.selectedWidget.model);
+
+        $scope.$apply(function () {
+          $scope.widgets.splice(index, 1);
+          $scope.selectedWidget = null;
+        });
+      }
+
+    });
+
   }]);
